5a5f496aa30b638ac5b48118478a933053895c76
yarikthree <djemzee@inbox.ru>
2023-10-22 14:13:57 +0300

    Клон репрозитория на мак

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP5K1Square1point25() {
+        double expected = 1.25;
+        int p = 5;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP5K2Square3point33() {
+        double expected = 3.33;
+        int p = 5;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP2K1Square0point5() {
+        double expected = 0.50;
+        int p = 2;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP23K8Square81point77() {
+        double expected = 81.77;
+        int p = 23;
+        double k = 8;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}

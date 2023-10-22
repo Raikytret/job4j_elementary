b11928edc1125fb2dafd91b16ce96ee9602139cd
yarikthree <djemzee@inbox.ru>
2023-10-22 14:15:04 +0300

    Клон репрозитория на мак

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,28 +7,33 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
-        double out = a.distance(b);
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void when10to21then1point41() {
-        double expected = 1.41;
-        Point a = new Point(1, 0);
-        Point b = new Point(2, 1);
-        double out = a.distance(b);
+    void when10to21then2() {
+        double expected = 1;
+        int x1 = 2;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when22to22then2() {
         double expected = 0;
-        Point a = new Point(2, 2);
-        Point b = new Point(2, 2);
-        double out = a.distance(b);
+        int x1 = 2;
+        int y1 = 2;
+        int x2 = 2;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
-
 }

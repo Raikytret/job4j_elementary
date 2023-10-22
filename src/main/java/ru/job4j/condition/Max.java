36d5339e06ee1dc372713635fcb1e3f92383c3a5
yarikthree <djemzee@inbox.ru>
2023-10-22 18:20:51 +0300

    Revert "4.2. Тернарное сравнение [#189]"
    This reverts commit bcdbea3966150c458ba3b139da477ee93e4612ff.

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean number = left > right;
-        int result = number ? left : right;
+        boolean number1 = left > right;
+        int result = number1 ? left : right;
         return result;
     }
 }

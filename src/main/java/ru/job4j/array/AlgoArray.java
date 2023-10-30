25f646a37a81c32bd3e3400a8960311857569770
yarikthree <djemzee@inbox.ru>
2023-10-30 21:41:30 +0300

    6.2. Упорядочить массив. [#4441 #456140] использовал одну переменную temp

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -5,12 +5,12 @@ public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
         int temp = array[0];
-        int temp1 = array[1];
         array[0] = array[3];
-        array[1] = array[2];
-        array[2] = temp1;
         array[3] = array[4];
         array[4] = temp;
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
 
         for (int i : array) {
             System.out.println(i);

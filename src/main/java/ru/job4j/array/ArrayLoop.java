d0857d9691aefee46b591dfe865019d85a0a1fca
yarikthree <djemzee@inbox.ru>
2023-10-30 21:26:50 +0300

    6.0.4. Массивы и цикл for. [#226665 #456132]  переименовал массив с y на numbers

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class ArrayLoop {
 
     public static void main(String[] args) {
-        int[] y = new int[5];
-        for (int i = 0; i < y.length; i++) {
-            y[i] = i * 2 + 3;
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 2 + 3;
         }
-        for (int j : y) {
+        for (int j : numbers) {
             System.out.println(j);
         }
     }

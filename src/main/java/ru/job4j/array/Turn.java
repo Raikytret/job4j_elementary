2df45399f73bb4af2e2fa0852c053bdd2f669196
yarikthree <djemzee@inbox.ru>
2023-10-30 21:49:29 +0300

    6.2.3. Перевернуть массив. [#226790 #456155]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,29 +2,11 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        if (array.length % 2 == 0) {
-            for (int i = 0; i < array.length - 1; i++) {
-                int temp = array[0];
-                array[0] = array[array.length - 1];
-                array[array.length - 1] = temp;
-                for (int k = 0; k < array.length - 1; k++) {
-                    int temp1 = array[0 + 1];
-                    array[0 + 1] = array[array.length - 1 - 1];
-                    array[array.length - 1 - 1] = temp1;
-                }
+            for (int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - 1 - i];
+                array[array.length - 1 - i] = temp;
             }
-        } else {
-            for (int i = 0; i < array.length; i++) {
-                int temp = array[0];
-                array[0] = array[array.length - 1];
-                array[array.length - 1] = temp;
-                for (int k = 0; k < array.length; k++) {
-                    int temp1 = array[0 + 1];
-                    array[0 + 1] = array[array.length - 1 - 1];
-                    array[array.length - 1 - 1] = temp1;
-                }
-            }
-        }
         return array;
     }
 }

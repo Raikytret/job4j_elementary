af382910374f41c11afd8b08a8e1a1b342c71e7a
yarikthree <djemzee@inbox.ru>
2024-01-05 12:55:45 +0300

    6.7.1. Моно строка в матрице. [#214126 #457561] убрал цикл for

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,13 +3,10 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        char r =  'X';
-        for (int i = row; i <= row; i++) {
-            for (int k = 0; k < board[i].length; k++) {
-                if (board[i][k] != r) {
+            for (int k = 0; k < board.length; k++) {
+                if (board[row][k] != 'X') {
                     result = false;
                     break;
-                }
             }
         }
         return result;
